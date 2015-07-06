19767a7a007037049a56358d8411b53d69e8f9ee
kiranw <kiranw@mit.edu>
2015-07-05 21:22:33 -0400

    Buttons

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,12 +1,46 @@
+
+// Bounce sets the behavior of the arrow in the splash section
 function bounce() {
 	$('#down').addClass("animated bounce");
 	setTimeout(removeBounce,700);
 }
 
+// Removes bounce animation from arrow in splash
 function removeBounce() {
 	$('#down').removeClass("animated bounce");
 }
 
-function toggleLiveInput() {
-	console.log('working');
+// Begins recording audio
+function recordStart() {
+	changeButtons("record");
 }
+
+// Stops recording
+function stopRecord() {
+	changeButtons("stop");
+}
+
+// Playback audio
+function playRecord() {
+	changeButtons("play");
+}
+
+// Download .wav file
+function downloadRecord() {
+	changeButtons("download");
+}
+
+// Updates button display for which button should be active (can also deactivate all buttons if ID isnt equal to anything)
+function changeButtons(activeID) {
+	var path = "/static/img/";
+	var buttons = {"record":"recording.png", "play":"playing.png", "download":"downloading.png", "stop":"stopping.png"};
+	for (button in buttons) {
+		if (button == activeID) {
+			document.getElementById(button).src=path+buttons[button];
+		}
+		else {
+			document.getElementById(button).src=path+button+".png";
+		}
+	}
+}
+
